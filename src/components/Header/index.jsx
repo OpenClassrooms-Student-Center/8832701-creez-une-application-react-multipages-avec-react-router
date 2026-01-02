134530df2f229076590ec23dd01095055bfe2788
Aurelien Antonio <aurelien@kickersoft.com>
2026-01-02 00:15:37 -0500

    Integrate React Router and update navigation to support multiple pages in Shiny app

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,5 @@
+import { Link } from 'react-router-dom'
 import './Header.css'
-
-// Note pour l'étudiant : Ce composant Header contient la structure de base.
-// Dans le chapitre 1, vous remplacerez les balises <a> par des composants <Link> de React Router.
 
 function Header() {
   return (
@@ -9,11 +7,9 @@ function Header() {
       <div className="header-content">
         <h1 className="header-logo">Shiny</h1>
         <nav className="nav">
-          {/* Ces liens sont des ancres qui scrollent dans la page.
-              Dans le chapitre 1, ils seront remplacés par des composants Link pour naviguer entre les pages */}
-          <a href="#dashboard" className="nav-link">Dashboard</a>
-          <a href="#projects" className="nav-link">Projets</a>
-          <a href="#employees" className="nav-link">Employés</a>
+          <Link to="/" className="nav-link">Dashboard</Link>
+          <Link to="/projects" className="nav-link">Projets</Link>
+          <Link to="/employees" className="nav-link">Employés</Link>
         </nav>
       </div>
     </header>
