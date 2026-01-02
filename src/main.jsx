e5504c98ed59f9b8c4fc9675e72d4d55a0c317f5
Aurelien Antonio <aurelien@kickersoft.com>
2026-01-02 09:04:12 -0500

    Refactor routing structure by introducing Layout component for consistent page layout and navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import ProjectDetail from './pages/ProjectDetail'
@@ -14,15 +14,16 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path="/employees" element={<Employees />} />
-        <Route path="/employees/:id" element={<EmployeeDetail />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/employees/:id" element={<EmployeeDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </Router>
   </StrictMode>
