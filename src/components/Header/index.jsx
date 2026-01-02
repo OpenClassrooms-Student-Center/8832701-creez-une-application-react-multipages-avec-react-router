d4e430ecf133e2264dfee7352c6f038c60c421fb
Aurelien Antonio <aurelien@kickersoft.com>
2026-01-02 09:13:21 -0500

    Add About page with nested routing and updated navigation styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 function Header() {
@@ -7,10 +7,10 @@ function Header() {
       <div className="header-content">
         <h1 className="header-logo">Shiny</h1>
         <nav className="nav">
-          <Link to="/" className="nav-link">Dashboard</Link>
-          <Link to="/projects" className="nav-link">Projets</Link>
-          <Link to="/employees" className="nav-link">Employés</Link>
-          <Link to="/about" className="nav-link">À propos</Link>
+          <NavLink to="/" end className="nav-link">Dashboard</NavLink>
+          <NavLink to="/projects" className="nav-link">Projets</NavLink>
+          <NavLink to="/employees" className="nav-link">Employés</NavLink>
+          <NavLink to="/about" className="nav-link">À propos</NavLink>
         </nav>
       </div>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,10 @@ import Projects from './pages/Projects'
 import ProjectDetail from './pages/ProjectDetail'
 import Employees from './pages/Employees'
 import EmployeeDetail from './pages/EmployeeDetail'
-import About from './pages/About'
+import AboutLayout from './pages/About/Layout'
+import AboutPresentation from './pages/About/Presentation'
+import AboutTeam from './pages/About/Team'
+import AboutContact from './pages/About/Contact'
 import Error from './pages/Error'
 import './index.css'
 
@@ -21,7 +24,11 @@ createRoot(document.getElementById('root')).render(
           <Route path="/projects/:id" element={<ProjectDetail />} />
           <Route path="/employees" element={<Employees />} />
           <Route path="/employees/:id" element={<EmployeeDetail />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<AboutLayout />}>
+            <Route index element={<AboutPresentation />} />
+            <Route path="team" element={<AboutTeam />} />
+            <Route path="contact" element={<AboutContact />} />
+          </Route>
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
